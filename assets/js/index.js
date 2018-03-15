4aa277eb34e951d07fcd15f5e81a046f6b443057
josephemswiler <josephemswiler@gmail.com>
2018-03-15 17:55:04 -0500

    add jQuery, Firebase

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,17 @@
+$(document).ready(function () {
+
+    var config = {
+        apiKey: "AIzaSyD-C8fCk0JV3EVUlXQHGLLpLlg8HJI_Tbw",
+        authDomain: "project-1-jfe.firebaseapp.com",
+        databaseURL: "https://project-1-jfe.firebaseio.com",
+        projectId: "project-1-jfe",
+        storageBucket: "project-1-jfe.appspot.com",
+        messagingSenderId: "38105637620"
+    };
+    firebase.initializeApp(config);
 
 
-$( document ).ready(function(){
-
-$(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
 
 
 })
