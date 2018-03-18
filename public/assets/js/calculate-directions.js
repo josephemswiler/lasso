618bb1c93c1e9251d6444c6af4a174db49235ca5
Malazanian <irutner22@gmail.com>
2018-03-18 15:46:41 -0500

    build google maps step-by-step nav link from URI encoded string using chosen route

--- public/assets/js/calculate-directions.js
+++ public/assets/js/calculate-directions.js
@@ -3,6 +3,7 @@ let addDestination = new Vue({
     data: {
         waypoints: [],
         totalDistance: 0,
+        routeResult: {},
     },
     methods: {
         addDest: function (placeId) {
@@ -34,7 +35,7 @@ let addDestination = new Vue({
                 destination: end,
                 waypoints: this.waypoints,
                 optimizeWaypoints: true,
-                travelMode: 'WALKING'
+                travelMode: 'WALKING',
             };
             directionsService.route(request, function (result, status) {
                 console.log("This is the result: ", result);
@@ -42,7 +43,9 @@ let addDestination = new Vue({
                 if (status == 'OK') {
                     directionsDisplay.setDirections(result);
                     console.log("Directions results: ", result)
+                    addDestination.routeResult = result;
                     computeTotalDistance.computeTotalDistance(result);
+                    googleMapsLink.buildMapsLink();
                 }
             });
         }
@@ -52,7 +55,7 @@ let addDestination = new Vue({
 let computeTotalDistance = new Vue({
     el: '#total-distance',
     data: {
-        totalDistance: 0,
+        totalDistance: "",
     },
     methods: {
         computeTotalDistance(result) {
@@ -61,8 +64,52 @@ let computeTotalDistance = new Vue({
             for (let i = 0; i < myroute.legs.length; i++) {
               total += myroute.legs[i].distance.value;
             }
-            total = total / 1000;
-            this.totalDistance = "Total trip distance: " + total + " km";
-          }
+            // Convert distance.value (always in meters) to miles
+            function getMiles(meters) {
+                return (meters * 0.000621371192).toFixed(2);
+            }
+            total = getMiles(total);
+            this.totalDistance = "Total trip distance: " + total + " miles";
+        }
     }
-})
+});
+
+// Build a link to open step-by-step navigation in google maps with the selected route:
+let googleMapsLink = new Vue({
+    el: '#google-maps-link',
+    data: {
+        addedWaypoints: "&waypoints=",
+        addedWaypointPlaceIds: "&waypoint_place_ids=",
+        googleMapsBuiltLink: "https://www.google.com/maps/dir/?api=1&origin=",
+    },
+    methods: {
+        buildMapsLink() {
+            let startingPoint = addDestination.routeResult.routes["0"].legs["0"].start_address;
+            let endingPoint = "&destination=" + addDestination.routeResult.routes["0"].legs["0"].start_address + "&travelmode=walking";
+            // Iterate through the routes and build the waypoint address string
+            for (let i = 0; i < addDestination.routeResult.routes["0"].legs.length; i++) {
+                this.addedWaypoints += encodeURIComponent(addDestination.routeResult.routes["0"].legs[i].start_address)
+                // Separate each waypoint with a pipe, URI encoded
+                if (i < addDestination.routeResult.routes["0"].legs.length - 1) {
+                    this.addedWaypoints += "%7"
+                }
+            }
+            // Iterate through the waypoints and build the placeId string
+            for (let j = 0; j < addDestination.waypoints.length; j++) {
+                this.addedWaypointPlaceIds += addDestination.waypoints[j].location.placeId;
+                // Separate each placeId with a pipe, URI encoded
+                if (j < addDestination.waypoints.length - 1) {
+                    this.addedWaypointPlaceIds += "%7";
+                }
+            }
+            // Add the starting point, waypoint address strings, and waypoint placeId strings, and destination
+            this.googleMapsBuiltLink += startingPoint + endingPoint + this.addedWaypoints + this.addedWaypointPlaceIds
+        }
+    }
+});
+
+// origin=Paris,France&destination=Cherbourg,France&travelmode=driving
+// &waypoints=Versailles,France%7CChartres,France%7CLe+Mans,France%7CCaen,France
+// &waypoint_place_ids=ChIJdUyx15R95kcRj85ZX8H8OAU%7CChIJKzGHdEgM5EcR_OBTT3nQoEA%7CChIJG2LvQNCI4kcRKXNoAsPi1Mc%7CChIJ06tnGbxCCkgRsfNjEQMwUsc
+
+// let googleMapsLink = "https://www.google.com/maps/dir/?api=1&origin=" + startingPoint
