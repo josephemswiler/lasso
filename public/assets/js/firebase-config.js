07209585705346d3983553927b199a7a5c31b85a
josephemswiler <josephemswiler@gmail.com>
2018-03-20 18:21:33 -0500

    updates

---
+++ public/assets/js/firebase-config.js
@@ -0,0 +1,25 @@
+(function () {
+
+    var config = {
+        apiKey: "AIzaSyD-C8fCk0JV3EVUlXQHGLLpLlg8HJI_Tbw",
+        authDomain: "project-1-jfe.firebaseapp.com",
+        databaseURL: "https://project-1-jfe.firebaseio.com",
+        projectId: "project-1-jfe",
+        storageBucket: "project-1-jfe.appspot.com",
+        messagingSenderId: "38105637620"
+    };
+    firebase.initializeApp(config);
+    
+//users
+    //id
+        //name
+        //places
+            //google place id
+        //locateOn: false
+        //isActive: false
+        //search
+            //cost
+            //time
+            //stops add button, close button
+
+})()
