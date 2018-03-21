24e253c8d280b3c3c14aea553f4ae54100972680
josephemswiler <josephemswiler@gmail.com>
2018-03-20 19:29:17 -0500

    firebase init

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -9,6 +9,43 @@
         messagingSenderId: "38105637620"
     };
     firebase.initializeApp(config);
+
+    let database = firebase.database();
+    let currentUserName = "";
+    let currentUserTuple = new Object();
+  
+    let savedLocalUsers = JSON.parse(localStorage.getItem('localSavedUsers'));
+    if (!Array.isArray(savedUserNames)) {
+      savedUserNames = [];
+    }
+
+    $('.google-login').click(function () {
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+    
+          let name = user.email.substr(0, user.email.indexOf('@'));
+    
+          currentUserName = name;
+          
+          addUser(currentUserName);
+          // ...
+        }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      })
     
 //users
     //id
